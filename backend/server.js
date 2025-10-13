6a772e66c9eb487a5ffca120f419672716affc69
root <root@ADAPTER-INFRA1.ad.adapter.local>
2025-10-13 17:23:32 -0300

    test: Testa falha no health check

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
   } else {
